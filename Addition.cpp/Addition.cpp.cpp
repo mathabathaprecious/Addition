28649756078ea16feb40ed8f0ddb8c1b96a9c1c6
mathabathaprecious <mpuruprecious21@gmail.com>
2022-05-24 08:22:46 +0200

    Add project files.

---
+++ Addition.cpp/Addition.cpp.cpp
@@ -0,0 +1,41 @@
+// Addition.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //declaring variables
+    int firstNumber = 12;
+    int secondNumber = 6;
+    int sum = 0;
+    //prompt user to enter two numbers;
+
+    cout << "enter your firstNumber" << endl;
+    cin >> firstNumber;
+
+    cout << "enter your secondNumber" << endl;
+    cin >> secondNumber;
+
+    sum = firstNumber + secondNumber;
+
+    cout << " the sum of " << firstNumber << " and " << secondNumber << " equals to " << sum;
+
+
+    return 0;
+
+
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
